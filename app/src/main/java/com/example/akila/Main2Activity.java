f704c2dcbc01eb5a297018768b3b1932ba09ca84
Ravindu <ravinduamaya@gmail.com>
2020-03-19 14:52:45 +0530

    1 commit

--- app/src/main/java/com/example/akila/Main2Activity.java
+++ app/src/main/java/com/example/akila/Main2Activity.java
@@ -1,16 +1,14 @@
 package com.example.akila;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class stu_login extends AppCompatActivity {
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.stu_login);
+        setContentView(R.layout.activity_main2);
     }
-
-
 }
