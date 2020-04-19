fd09641ef698f47423cc478d8f488bd6f53a329b
thisuraR <61408031+thisuraR@users.noreply.github.com>
2020-04-19 22:20:57 +0530

    Activity classes updated

--- app/src/main/java/com/example/UserApp/RatingActivity.java
+++ app/src/main/java/com/example/UserApp/RatingActivity.java
@@ -7,20 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Main4Activity extends AppCompatActivity {
+public class RatingActivity extends AppCompatActivity {
 
     Button secondbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main4);
+        setContentView(R.layout.activity_rating);
 
         //this onclick is used to move to the Main3Activity
         secondbutton = findViewById(R.id.button5);
         secondbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent2 = new Intent(Main4Activity.this, Main3Activity.class);
+                Intent intent2 = new Intent(RatingActivity.this, TeacherListActivity.class);
                 startActivity(intent2);
             }
         });
